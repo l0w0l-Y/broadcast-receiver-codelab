a8c0e15965c204bf2b516c3d767bbdb45def5675
Aleksandra <portos100.00.1a@gmail.com>
2022-04-25 14:25:25 +0300

    Solution

---
+++ app/src/main/java/com/kaleksandra/broadcasts/codelab/CustomReceiver.kt
@@ -0,0 +1,30 @@
+package com.kaleksandra.broadcasts.codelab
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.widget.Toast
+import com.kaleksandra.broadcasts.codelab.CustomReceiver.Companion.ACTION_CUSTOM_BROADCAST
+
+class CustomReceiver : BroadcastReceiver() {
+    companion object {
+        const val ACTION_CUSTOM_BROADCAST =
+            BuildConfig.APPLICATION_ID + ".ACTION_CUSTOM_BROADCAST"
+        const val RANDOM_EXTRA = "RANDOM_EXTRA"
+    }
+
+    override fun onReceive(context: Context, intent: Intent) {
+        val intentAction = intent.action
+        if (intentAction != null) {
+            var toastMessage = "unknown intent action"
+            when (intentAction) {
+                Intent.ACTION_POWER_CONNECTED -> toastMessage = "Power connected!"
+                Intent.ACTION_POWER_DISCONNECTED -> toastMessage = "Power disconnected!"
+                ACTION_CUSTOM_BROADCAST -> {
+                    toastMessage = intent.getIntExtra(RANDOM_EXTRA, 0).toString()
+                }
+            }
+            Toast.makeText(context, toastMessage, Toast.LENGTH_SHORT).show()
+        }
+    }
+}
--- app/src/main/java/com/kaleksandra/broadcasts/codelab/MainActivity.kt
+++ app/src/main/java/com/kaleksandra/broadcasts/codelab/MainActivity.kt
@@ -1,11 +1,49 @@
 package com.kaleksandra.broadcasts.codelab
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.content.IntentFilter
 import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import androidx.localbroadcastmanager.content.LocalBroadcastManager
+import com.kaleksandra.broadcasts.codelab.CustomReceiver.Companion.ACTION_CUSTOM_BROADCAST
+import com.kaleksandra.broadcasts.codelab.CustomReceiver.Companion.RANDOM_EXTRA
+import kotlin.random.Random
+
 
 class MainActivity : AppCompatActivity() {
+    private val mReceiver: CustomReceiver = CustomReceiver()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val filter = IntentFilter()
+        filter.addAction(Intent.ACTION_POWER_DISCONNECTED)
+        filter.addAction(Intent.ACTION_POWER_CONNECTED)
+        LocalBroadcastManager.getInstance(this)
+            .registerReceiver(
+                mReceiver,
+                IntentFilter(ACTION_CUSTOM_BROADCAST)
+            )
         setContentView(R.layout.activity_main)
+        // Register the receiver using the activity context.
+        this.registerReceiver(mReceiver, filter)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        findViewById<Button>(R.id.sendBroadcast).setOnClickListener {
+            val customBroadcastIntent = Intent(ACTION_CUSTOM_BROADCAST)
+            val rand = (0..10).random(Random(System.currentTimeMillis()))
+            customBroadcastIntent.putExtra(RANDOM_EXTRA, rand)
+            LocalBroadcastManager.getInstance(this).sendBroadcast(customBroadcastIntent)
+        }
+    }
+
+    override fun onDestroy() {
+        //Unregister the receiver
+        this.unregisterReceiver(mReceiver)
+        LocalBroadcastManager.getInstance(this)
+            .unregisterReceiver(mReceiver)
+        super.onDestroy()
     }
 }
